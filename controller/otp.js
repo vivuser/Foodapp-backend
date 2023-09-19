9719657fbe4ea043658b85cae851307185d5b3ca
vivuser <vivek>
2023-09-19 18:48:52 +0530

    otp1

--- controller/otp.js
+++ controller/otp.js
@@ -1,2 +1,16 @@
 const UserModal = require('../model/user');
+const UserModel = require('../model/userModel');
 const sendEmail = require('../Config/emailService');
+
+exports.sendOTP = async (req, res) => {
+
+     try {
+        const { email } = req.body;
+        const otp = generateRandomOTP();
+        const expirationTime = new Date();
+        expirationTime.setMinutes(expirationTime.getMinutes() + 5);
+
+
+        await UserMOdel.updateOne({ email }, { otp, otpExpiration: expirationTime });
+     }
+}
